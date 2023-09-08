f3539ab4425d7b8c86f7411d9c2759c69fd98d17
Funasitien DreamCloud <86372093+Funasitien@users.noreply.github.com>
2023-09-08 22:00:28 +0200

    🚑 Still not fixed. Try ChatGPT

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,38 @@ module.exports = {
     require.resolve('@docusaurus/core/lib/babel/preset'),
     "@babel/preset-react",
     "@babel/preset-env"
+    ],
+
+  module: {
+    "rules": [
+      {
+        "test": /\.(js|jsx)$/,
+        "exclude": /node_modules/,
+        "use": {
+          "loader": "babel-loader",
+          "options": {
+            // ... your existing Babel options
+          }
+        }
+      },
+      // Add the babel-macros loader here
+      {
+        "test": /\.(js|jsx)$/,
+        "exclude": /node_modules/,
+        "use": {
+          "loader": "babel-loader",
+          "options": {
+            // ... your options specific to babel-macros, if any
+          }
+        }
+      },
+      // Add any other loaders you need for handling various file types here
+      {
+        "test": /\.css$/,
+        "use": ["style-loader", "css-loader"]
+      }
     ]
+  }
 };
 
 module.exports = function (api) {
