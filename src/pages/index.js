58c3cddfc7eea32f3158b15faa58b02ca45354f5
Funasitien DreamCloud <86372093+Funasitien@users.noreply.github.com>
2023-09-08 19:59:19 +0200

    🚧 First try with FontAwesome

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,16 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
-  return (
+  return ( 
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
+        <FontAwesomeIcon icon={icon({name: 'discord', style: 'brands'})} />
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
